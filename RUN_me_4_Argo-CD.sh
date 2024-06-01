02f11bc84b71d8c1c94250a04a41840f5d02c51e
vjohn <perljohn@gmail.com>
2024-06-01 14:29:06 +0530

    add

--- RUN_me_4_Argo-CD.sh
+++ RUN_me_4_Argo-CD.sh
@@ -1,22 +1,37 @@
 
-
-
-echo "You are going to start   "
-
-
 #!/bin/bash
+	echo "You are going to start   "
+       	echo "Welcome $(whoami)"
+        echo "You are using $(cat /etc/issue)"
 
+                # Check if the first argument (tag) is provided
+    if [ -z "$1" ]; then
+        echo "Error: Tag is required. Usage: Demo_argo-cd_Full_Install <tag>"
+        return 1
+    fi
+
+        a="$1"
+	echo "Tag is $1"
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$0")
-
-# Call other scripts using relative paths
-# If the other scripts are in the same directory
+echo -e "$SCRIPT_DIRi"
+pwd
+## echo  -e " Going to instrall docker If not installed   " 
 "$SCRIPT_DIR/ArgoCD_Shell-Scripts/docker-install.sh"
+## echo  -e "  Going to instrall kubectl If not installed  " 
 
-# If the other scripts are in a subdirectory called "scripts"
 "$SCRIPT_DIR/ArgoCD_Shell-Scripts/kubectl-install.sh"
-
-# If the other scripts are in a parent directory
+## echo  -e "  Going to instrall minikube  If not installed  "
 "$SCRIPT_DIR/ArgoCD_Shell-Scripts/minikube-install.sh"
+
+## echo  -e "  Going to instrall helm-install  If not installed  "
+"$SCRIPT_DIR/ArgoCD_Shell-Scripts/helm-install.sh"
+
+
+
+
+
+
+
 
 
