4b31d4dc9f42d7dc30b993a90cfbec1288302149
vjohn <perljohn@gmail.com>
2024-06-01 17:39:57 +0530

    Add repi to Argo cd

--- ArgoCD_Shell-Scripts/Install_Argo-CD_in_kubectl.sh
+++ ArgoCD_Shell-Scripts/Install_Argo-CD_in_kubectl.sh
@@ -53,36 +53,3 @@
         echo -e " The below can be done Via UI also   "
 
 ######################################################################################
-#
-#
-#echo -e " to add repo to argo    argocd repo add https://github.com/panampunna/Helm-Kubectl-demo.git "
-#        argocd repo add https://github.com/panampunna/Helm-Kubectl-demo.git
-#        echo -e "  argocd repo list "
-#        argocd repo list
-#
-#
-#	        echo -e " argocd app create argocd-app-ui --repo  https://github.com/panampunna/Helm-Kubectl-demo.git  --path Demo_argo-cd_helm-chart-ui/ --dest-server https://kubernetes.default.svc --dest-namespace default  "
-#        argocd app create demo-argo-helm-ui --repo  https://github.com/panampunna/Helm-Kubectl-demo.git  --path demo-argo-helm-ui/ --dest-server https://kubernetes.default.svc --dest-namespace default
-#        argocd app get demo-argo-helm-ui
-#        argocd app sync demo-argo-helm-ui
-#        argocd app list
-#
-####################################################################
-#        echo -e "\n\n\n\n \n\n\n\n \n\n\n\n \n\n\n\n     "
-#
-############################
-#              echo -e " argocd app create argocd-app-db --repo  https://github.com/panampunna/Helm-Kubectl-demo.git  --path Demo_argo-cd_helm-chart-db/ --dest-server https://kubernetes.default.svc --dest-namespace default  "
-#        argocd app create demo-argo-helm-db --repo  https://github.com/panampunna/Helm-Kubectl-demo.git  --path demo-argo-helm-db/ --dest-server https://kubernetes.default.svc --dest-namespace default
-#        argocd app get demo-argo-helm-db
-#        argocd app sync demo-argo-helm-db
-#        argocd app list
-#
-#        echo -e " argocd  UI  ################################### n\\n "
-#        chromium-browser http://localhost:8080/login  &
-#        minikube tunnel &
-#        kubectl get service -A &
-#
-#
-###}
-#
-#
