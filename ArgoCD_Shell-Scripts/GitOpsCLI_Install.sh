f82ccd54490ed3864055bb9175f263796c862ed9
vjohn <perljohn@gmail.com>
2024-06-03 10:45:32 +0530

    path PWD added

--- ArgoCD_Shell-Scripts/GitOpsCLI_Install.sh
+++ ArgoCD_Shell-Scripts/GitOpsCLI_Install.sh
@@ -6,9 +6,11 @@
         echo " https://baloise.github.io/gitopscli/setup/  "
         echo    "https://www.youtube.com/watch?v=f5EpcWp0THw "
 	sudo apt install python3-poetry  -y 
+	pwd 
         git clone https://github.com/baloise/gitopscli.git
         cd gitopscli/
         poetry install
+	pwd 
         poetry run gitopscli --help
         #       sudo apt install -y  python3-pip pipx python3.12
         #       cd  /tmp
